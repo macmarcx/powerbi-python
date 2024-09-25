f0205678ac228964ef50f2c50a95ffe2ba45ed6e
Marcos Silva <marcosilva968@gmail.com>
2024-09-25 14:05:01 +0100

    power BI + Python

---
+++ Scripts/script_read_SQL.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+
+with sqlite3.connect(r"C:\Users\marco\Documents\powerbi-python\Scripts\car_sales.db") as connection:
+    df = pd.read_sql_query("SELECT * FROM sales", connection)
+
+cars = df[
+    [
+        "vin",
+        "car",
+        "mileage",
+        "license",
+        "color",
+        "purchase_date",
+        "purchase_price",
+        "investment",
+    ]
+]
+customers = df[["vin", "customer"]]
+sales = df[["vin", "sale_price", "sale_date"]]
